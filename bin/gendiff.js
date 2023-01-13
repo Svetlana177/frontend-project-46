9668ea41122501f79ab1681524132d9b8e4aad5d
Svetlana177 <svetlanarozhkova177@mail.ru>
2023-01-13 18:23:32 +0500

    added task 3: Flat File Comparison (JSON)

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-
-const { Command } = require('commander');
+import { Command } from 'commander';
+import genDiff from '../src/index.js';
 
 const program = new Command();
 
@@ -8,6 +8,9 @@ program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format');
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => {
+      console.log(genDiff(filepath1, filepath2));
+  });
 
 program.parse();
---
+++ src/index.js
@@ -0,0 +1,40 @@
+import path from 'path';
+import _ from 'lodash';
+import fs from 'fs';
+
+const genDiff = (filepath1, filepath2) => {
+  const current = process.cwd();
+  const path1 = fs.readFileSync(path.resolve(current, filepath1), 'utf-8');
+  const parseFilepath1 = JSON.parse(path1);
+  const path2 = fs.readFileSync(path.resolve(current, filepath2), 'utf-8');
+  const parseFilepath2 = JSON.parse(path2);
+  const sortMass = _.sortBy([...Object.entries(parseFilepath1), ...Object.entries(parseFilepath2)]);
+  const entries = Object.keys(Object.fromEntries(sortMass));
+  let result = '';
+
+  const mainFunction = (key) => {
+    if (_.has(parseFilepath1, key) && _.has(parseFilepath2, key)) {
+      if (parseFilepath1[key] === parseFilepath2[key]) {
+        result = `  ${key}: ${parseFilepath1[key]}`;
+        return result;
+      }
+      if (parseFilepath1[key] !== parseFilepath2[key]) {
+        result = `- ${key}: ${parseFilepath1[key]}
++ ${key}: ${parseFilepath2[key]}`;
+        return result;
+      }
+    }
+    if (_.has(parseFilepath1, key)) {
+      result = `- ${key}: ${parseFilepath1[key]}`;
+      return result;
+    }
+
+    result = `+ ${key}: ${parseFilepath2[key]}`;
+    return result;
+  };
+
+  const difference = entries.map((elem) => mainFunction(elem));
+  return `{\n ${difference.join('\n')} \n}`;
+};
+
+export default genDiff;
